476bdfcf434a65b59ab300fab7e343c3dc003c10
Bolaolat <bolaolat300@gmail.com>
2024-11-30 10:08:12 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 
-global.pairingNumber = "2349118736310"
+global.pairingNumber = "2347057344983"
  
-global.botNumber = ["2349118736310"]
+global.botNumber = ["2347057344983"]
 
 global.sign = "✘"
 // ☞ ➻ ➽ ☛ ✔
