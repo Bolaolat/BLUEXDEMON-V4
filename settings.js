2cf8263728966f80175724f19170ccb6a077de04
Bolaolat <bolaolat300@gmail.com>
2024-12-04 23:10:52 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 
-global.pairingNumber = "2349118736310"
+global.pairingNumber = "2349036325074"
  
-global.botNumber = ["2349118736310"]
+global.botNumber = ["2349036325074"]
 
 global.anticall = true
 
